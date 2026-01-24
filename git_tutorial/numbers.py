c07f99b5083f2406eeebe5592a21011750135b84
Maksatchs <justicefixw@gmail.com>
2026-01-24 10:57:35 +0500

    first commit

---
+++ git_tutorial/numbers.py
@@ -0,0 +1,44 @@
+#There are three numeric types in Python: int, float, and complex.
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#You can convert from one type to another with the int(), float(), and complex() functions:
+
+a = float(x) # convert int to float
+b = int(y)   # convert float to int
+c = complex(x) # convert int to complex
+print(a)
+print(b)
+print(c)
+
+#You can also use the type() function to find out what type a variable is:
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Note: In Python, the float type is a double-precision floating-point number.
+#Note: The complex type is used to represent complex numbers, which have a real and an imaginary part.
+
+x = 1
+y = 35656222554887711
+z = -3255522
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
